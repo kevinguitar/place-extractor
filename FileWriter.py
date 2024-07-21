cf0951c562dba78fa3c13f0ef6b8ad42893329a1
Kevin Chiu <kevin85619@gmail.com>
2024-07-21 16:15:34 +0800

    Improve file writing method

--- FileWriter.py
+++ FileWriter.py
@@ -6,15 +6,15 @@ from xlwt import Formula
 from xlwt import Workbook
 
 HEADER = [
-    '名稱',
-    '地址',
-    '距離',
-    '預估開車時間',
-    '電話',
-    '評分',
-    '評分人數',
-    '網站',
-    'Google Map連結'
+    'Name',
+    'Address',
+    'Distance',
+    'Drive time',
+    'Phone',
+    'Rating',
+    'Rates',
+    'Website',
+    'Google Map URL'
 ]
 COLUMN_WIDTH = [
     300,
@@ -41,17 +41,19 @@ def write_to_excel_and_open(places, filename):
         sheet.write(0, i, header, header_style)
         sheet.col(i).width = COLUMN_WIDTH[i] * 42
 
-    # todo: Improve hardcoded index
     for i, place in enumerate(places, start=1):
-        sheet.write(i, 0, place.get('name'))
-        sheet.write(i, 1, place.get('vicinity'))
-        sheet.write(i, 2, place.get('distance').get('text'))
-        sheet.write(i, 3, place.get('duration').get('text'))
-        sheet.write(i, 4, place.get('formatted_phone_number'))
-        sheet.write(i, 5, str(place.get('rating')))
-        sheet.write(i, 6, place.get('user_ratings_total'))
-        sheet.write(i, 7, __make_hyperlink(place.get('website')))
-        sheet.write(i, 8, __make_hyperlink(MAP_URL + place.get('place_id')))
+        for j, field in enumerate([
+            place.get('name'),
+            place.get('vicinity'),
+            place.get('distance').get('text'),
+            place.get('duration').get('text'),
+            place.get('formatted_phone_number'),
+            str(place.get('rating')),
+            place.get('user_ratings_total'),
+            __make_hyperlink(place.get('website')),
+            __make_hyperlink(MAP_URL + place.get('place_id'))
+        ]):
+            sheet.write(i, j, field)
 
     file_path = os.path.expanduser('~/Desktop/') + filename + '.xls'
     wb.save(file_path)
